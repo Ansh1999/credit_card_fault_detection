489b017d61b61dbd9b509d9e3d31090684619f42
Anshul Mehra <anshul.mehra5555@gmail.com>
2023-08-11 20:40:54 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,34 @@
+import os, sys
+from src.exception import CustomException
+from src.logger import logging
+from dataclasses import dataclass
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class DataIngestion:
+    def __init__(self):
+        self.train_path = os.path.join(os.getcwd(),'artifacts','train.csv')
+        self.test_path = os.path.join(os.getcwd(),'artifacts','test.csv')
+        self.raw_data = os.path.join(os.getcwd(),'artifacts','raw_data.csv')
+
+    def initiate_ingestion(self):
+        logging.info("Ingestion has initiated")
+        try:
+            df = pd.read_csv('notebook/data/kc_house_data.csv')
+            logging.info("Data is read successfuly...!!")
+
+            path = os.path.join(os.getcwd(),'artifacts')
+            os.makedirs(path,exist_ok=True)
+
+            train,test = train_test_split(df, test_size=0.25, random_state=42)
+            logging.info("Train test split is done successfully...!!!")
+            
+            train.to_csv(self.train_path,index=False,header=True)
+            test.to_csv(self.test_path,index=False,header=True)
+            df.to_csv(self.raw_data,index=False,header=True)
+            logging.info("All csv's have been saved to artifacts")
+            logging.info("Data ingestion is completed successfully")
+
+        except Exception as e:
+            raise CustomException(e,sys)
